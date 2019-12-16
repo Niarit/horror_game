60fc9daf5633b38934ca3d4e57f164b39b743229
peti <kalnaipeter@gmail.com>
2019-12-16 11:49:47 +0100

    ..

--- static/changeBackground.js
+++ static/changeBackground.js
@@ -16,11 +16,22 @@ function removeBtn(buttonName) {
     button.remove()
 }
 
+function collectItem(id,message,newId) {
+    let item = document.getElementById(id);
+    item.addEventListener('click',function () {
+        changeText(message);
+        item.setAttribute('id',newId);
+
+    })
+
+}
+
 function start() {
     let button = document.getElementById('btn');
     button.addEventListener('click', function () {
         changeBg("https://i.pinimg.com/originals/5c/71/f2/5c71f2087044c0eeb0cf8054964f1e54.jpg");
         changeText("Placeholder2");
+        collectItem()
         button.setAttribute('id', 'nextBtn');
         button.onclick = function () {
             changeBg("https://www.narcity.com/u/2019/09/13/e9518023ad39d20cb559c217e14f324.jpg_1200x630.jpg");
