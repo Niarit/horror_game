aa234db5771b9ff23f7bbc9dd5a0957986229392
nia <zsofi.deak.nia@gmail.com>
2019-12-16 10:07:57 +0100

    Add text box

--- static/changeBackground.js
+++ static/changeBackground.js
@@ -1,33 +1,43 @@
-let button = document.getElementById("btn");
-button.addEventListener("click", function () {
-    changeBg("https://i.pinimg.com/originals/5c/71/f2/5c71f2087044c0eeb0cf8054964f1e54.jpg")
-});
 
 function changeBg(background) {
     document.body.style.backgroundImage = 'url(' + background + ')';
     document.body.style.backgroundRepeat = "no-repeat";
     document.body.style.backgroundPosition = "center fix";
-    console.log(document.body.style.backgroundImage);
 }
 
+
+function changeText(newText) {
+    let text = document.getElementById("text");
+    text.innerHTML = newText
+}
 
 function removeBtn(buttonName) {
     let button = document.getElementById(buttonName);
-    button.parentNode.removeChild(button);
-    return false;
+    button.remove()
+}
+
+function start() {
+    let button = document.getElementById('btn');
+    button.addEventListener('click', function () {
+        changeBg("https://i.pinimg.com/originals/5c/71/f2/5c71f2087044c0eeb0cf8054964f1e54.jpg");
+        changeText("Placeholder2");
+        button.setAttribute('id', 'nextBtn');
+        button.onclick = function () {
+            changeBg("https://www.narcity.com/u/2019/09/13/e9518023ad39d20cb559c217e14f324.jpg_1200x630.jpg");
+            changeText("Placeholder3");
+            button.setAttribute('id', 'hallwayBtn');
+            let nextButton = document.getElementById("hallwayBtn");
+            nextButton.addEventListener('click',function () {
+                changeBg("https://wallpaperaccess.com/full/1371521.jpg");
+                changeText("Placeholder4");
+                let shatteringGlass = new Audio('../static/breaking-glass.mp3');
+                shatteringGlass.play();
+                nextButton.setAttribute('id', 'toTheEnd')
+            })
+        }
+    })
 }
 
 
-function nextButton(btnName) {
-    removeBtn('btn');
-    let nextBtn = document.createElement("BUTTON");
-    nextBtn.setAttribute("id", btnName);
-    nextBtn.innerHTML = "";
-    document.body.appendChild(nextBtn);
-    nextBtn.onclick = function () {
-        changeBg("https://www.narcity.com/u/2019/09/13/e9518023ad39d20cb559c217e14f324.jpg_1200x630.jpg");
-        removeBtn('nextBtn')
-    };
-
-}
+start();
 
