2170fa87954c720bf596602669666b0704708245
kalnaipeter <kalnaipeter@gmail.com>
2019-12-18 09:32:02 +0100

    new_game

--- app.py
+++ app.py
@@ -3,14 +3,19 @@ from flask import Flask, render_template, request, redirect, send_from_directory
 app = Flask(__name__)
 
 
-@app.route('/game')
+@app.route('/game-christmas')
 def point_and_click():
     return render_template('first_stage.html')
 
 
+@app.route('/game-new-year')
+def new_year_massacre():
+    return render_template('first_room.html')
+
+
 @app.route('/')
 def front_page():
-    return render_template('end_stage.html')
+    return render_template('front_page.html')
 
 
 @app.route('/end')
