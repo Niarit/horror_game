cf220900eaad496edb83d43b5ac475abd44de3e8
nia <zsofi.deak.nia@gmail.com>
2019-12-19 11:00:43 +0100

    Add back button to the end of new game

--- static/js/end_room_win.js
+++ static/js/end_room_win.js
@@ -5,3 +5,8 @@ let winButton= document.getElementById("win");
 winButton.onclick =function () {
     winModal.style.display ='block';
 };
+
+let backBtn = document.getElementById('back');
+backBtn.onclick = function () {
+    location.href = "http://127.0.0.1:5000"
+};
